c64a3cefe37d153dbdddb5a35dba518bd0be221f
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-06-23 19:23:09 +0600

    added route and component basic

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./Layout/MAinLayout.jsx";
+import Home from "./Components/Home.jsx";
+import AddCoffee from "./Components/AddCoffee.jsx";
+import Updatecoffee from "./Components/Updatecoffee.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        Component: <Home></Home>,
+      },
+      {
+        path: "addcoffe",
+        Component: <AddCoffee></AddCoffee>,
+      },
+      {
+        path: "uodatecoffee",
+        Component: <Updatecoffee></Updatecoffee>,
+      },
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
